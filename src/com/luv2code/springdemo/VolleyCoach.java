6167804e0e1dab4cfe943ccc85d3f947d01d1e94
albertohasegawa <albertohasegawa@gmail.com>
2019-09-09 13:30:21 -0300

    final commit

--- src/com/luv2code/springdemo/VolleyCoach.java
+++ src/com/luv2code/springdemo/VolleyCoach.java
@@ -1,13 +1,29 @@
 package com.luv2code.springdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class VolleyCoach implements Coach {
 
+	@Autowired
+	@Qualifier("happyFortuneService")
+	private FortuneService fortuneService;
+
+//	@Autowired
+//	public VolleyCoach(FortuneService theFortuneService) {
+//		fortuneService = theFortuneService;
+//	}
+	
 	@Override
 	public String getDailyWorkout() {
 		return "Train some serves";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		return fortuneService.getFortune();
 	}
 
 }
